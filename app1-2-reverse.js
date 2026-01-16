3e6f61120c82cd6b45dbadf5aa9ff568a8e5390d
Harry EMARH <emarh.harry.code@gmail.com>
2026-01-16 15:36:45 +0100

    Reverse array 2 3

--- app1-2-reverse.js
+++ app1-2-reverse.js
@@ -7,5 +7,8 @@ function reverseArray(arr) {
     return result;
 }
 
-const arr = [1, 2, 3, 4, 5];
+const arr = [6, 7, 8, 9, 10];
 console.log(reverseArray(arr));
+
+let tableau = [1, 2, 3, 4, 5];
+console.log(tableau[tableau.length-1]);
